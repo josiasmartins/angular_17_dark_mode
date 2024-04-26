e2db93610efa5325e5dda6cae640ac1c68fc556f
Caitano <josias.m.caitano@accenture.com>
2024-04-26 11:32:52 -0300

    config: add jest

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    present: "jest-preset-angular",
+    setupFilesAfterEnv: ['<rootDir>/src/app/setup.jest.ts'],
+        // '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+}
--- src/app/views/home/components/home.component.ts
+++ src/app/views/home/components/home.component.ts
@@ -1,34 +1,100 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output, input } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { NgbActiveModal, NgbAlert, NgbDatepickerModule, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'ngbd-modal-content',
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Hi there!</h4>
+      <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>
+    </div>
+    <div class="modal-body">
+      <p>Hello, {{ name }}!</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-outline-dark" (click)="test()">Close</button>
+    </div>
+
+   
+  `,
+})
+export class NgbdModalContent {
+
+  isActiveModal = true;
+
+  // @Output() isActiveModalEmit = new EventEmitter();
+
+  constructor(public activeModal: NgbActiveModal) {}
+
+  @Input() name!: string;
+
+  public test() {
+    this.activeModal.close('Close click');
+    this.isActiveModal = false;
+  }
+
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    NgbDatepickerModule,
+    NgbAlert,
+    // ModalModule.forRoot()
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
-  constructor(private router: Router) {}
+  public isActiveModal = true;
 
-  toogle(): void {
+  constructor(private router: Router, private modalService: NgbModal) {
+    this.open()
+  }
+
+  public name = "";
+  public modalRef!: NgbModalRef;
+
+  open(): void {
+    const modalRef = this.modalService.open(NgbdModalContent);
     
-    // const darkMode = document.getElementById("toogleDarkMode");
-
-    // darkMode?.addEventListener('click', () => {
-      document.body.classList.toggle('dark_mode')
-    // });
-
+    this.modalRef = modalRef;
+    modalRef.componentInstance.name = 'World';
+    this.name = modalRef.componentInstance.name;
   }
 
-  public navigateToLogin(): void {
-    this.router.navigate(['login'])
+  navigateToLogin(): void {
+    this.router.navigate(['login']);
   }
 
+  
+
+  @HostListener("document:click", ['$event']) 
+  test(event: any) {
+
+    console.log(this.modalRef.componentInstance, "ibag co")
+
+    const modal = (document.body.querySelector(".d-block modal .fade .show"))?.classList.contains("modal-open");
+    console.log(event.target, )
+    if (modal && this.modalRef.componentInstance.isActiveModal === true) {
+      // document.swrite("Removido o template")
+      // document.body.classList.remove("modal-open");
+
+      this.modalRef.close();
+
+      console.log(document.body.classList);
+      const inputEl = document.querySelector(".input_test") as HTMLInputElement;
+
+      console.log(inputEl, "ibag input")
+
+      inputEl.value = "";
+    }
+  }
 
 }
